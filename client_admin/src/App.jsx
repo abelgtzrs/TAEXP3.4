f614174179daff73af3020c283be4874a2585863
abelgtzrs <32408235+abelgtzrs@users.noreply.github.com>
2025-06-13 12:23:51 -0400

    Moved files to client_admin

---
+++ client_admin/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-teal-400">
+        Abel Experience Admin Panel
+      </h1>
+    </div>
+  );
+}
+export default App;
